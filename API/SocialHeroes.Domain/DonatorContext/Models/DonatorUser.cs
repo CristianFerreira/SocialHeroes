762bdbc339c3057c1d7a8a15ad8a645cf6b89d20
Cristian Ferreira <cristianferreira_gks@hotmail.com>
2018-10-14 18:22:34 -0300

    @CristianFerreira - #4 - To Implement FluentValidator - API

---
+++ API/SocialHeroes.Domain/DonatorContext/Models/DonatorUser.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidator;
+using System;
+
+namespace SocialHeroes.Domain.DonatorContext.Models
+{
+    public class DonatorUser :Notifiable
+    {
+        public DonatorUser(string firstName, string lastName, string CPF)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.CPF = CPF;
+        }
+
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CPF { get; set; }
+
+    }
+}
