48af2040cf55b517cddf2f3883258e105a1bdc7f
Cristian Ferreira <cristianferreira_gks@hotmail.com>
2019-08-21 22:04:50 -0300

    [+] - Add email service to send email

---
+++ src/SocialHeroes.Domain/Services/EmailService.cs
@@ -0,0 +1,20 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Net.Mail;
+
+namespace SocialHeroes.Domain.Services
+{
+    public class EmailService
+    {
+        public void SendEmail(string from, string to, string subject, string body)
+        {
+            var apiKey = Environment.GetEnvironmentVariable("sendGridKey");
+            var client = new SendGridClient(apiKey);
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(new EmailAddress(from), new EmailAddress(to), subject, plainTextContent, htmlContent);
+            client.SendEmailAsync(msg);
+        }
+    }
+}
